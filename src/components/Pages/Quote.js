e0f0dc3852c4a2c1829a81d6ba9650b628fb2ec3
Symphatico <Symphatico>
2021-05-19 21:57:27 -0700

    Visualizador de pdf (Falta)

--- src/components/Pages/Quote.js
+++ src/components/Pages/Quote.js
@@ -1,45 +1,16 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
+import Steps from '../Steps/Steps';
+import Pdf from '../Pdf/Pdf';
 //import { Parts } from '.././Parts/Parts';
 //import { Header, Grid, Button, Icon } from 'semantic-ui-react';
 
 export default class Quote extends Component {
   render() {
     return (
-      <div>
-        <div id='pasos-compra'>
-          <div class='ui steps'>
-            <div class=' completed step'>
-              <i class='cart icon'></i>
-              <div class='content'>
-                <div class='title'>Comprando</div>
-                <div class='description'>Elige tus componentes</div>
-              </div>
-            </div>
-            <div class='active step'>
-              <i class='thumbs up outline icon'></i>
-              <div class='content'>
-                <div class='title'>Confirma</div>
-                <div class='description'>Confirma tu orden</div>
-              </div>
-            </div>
-            <div class='disabled step'>
-              <i class='yen sign icon'></i>
-              <div class='content'>
-                <div class='title'>Listo para pagar</div>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div id='placeholder'>
-          <div class='ui placeholder segment'>
-            <div class='ui icon header'>
-              <i class='pdf file outline icon'></i>
-              el pedeefe va aki
-            </div>
-            <div class='ui primary button'>Add Document</div>
-          </div>
-        </div>
-      </div>
+      <Fragment>
+        <Steps />
+        <Pdf />
+      </Fragment>  
     );
   }
 }
