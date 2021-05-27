624f7dd3bed09bba3f5802806df5f749850b9897
IO <al18760117@ite.edu.mx>
2021-05-26 22:56:51 -0700

    💳Metodo de pago agregado

--- src/components/Ensamble/Ensamble.js
+++ src/components/Ensamble/Ensamble.js
@@ -1,166 +1,102 @@
-import React, { useState, useEffect, Fragment } from 'react';
-import { Button, Container, Label, Tab, Table } from 'semantic-ui-react';
+import React, { useState, useEffect } from 'react';
+import { Container, Tab } from 'semantic-ui-react';
 import { db } from '../../firebase';
 import { EnsambleContext } from './EnsambleContext';
 
-import logo from '../Navbar/giant.png';
 import Parts from './Parts';
 import Steps from '../Steps/Steps';
-
-import html2pdf from 'html2pdf.js';
+import Resumen from './Resumen';
 
 export default function Ensamble() {
-	let precioTotal = 0;
-	const [ensamble, setEnsamble] = useState({
-		cpu: '',
-		mb: '',
-		ram: '',
-		gpu: '',
-		hdd: '',
-		ssd: '',
-	});
+  const [ensamble, setEnsamble] = useState({
+    cpu: '',
+    mb: '',
+    ram: '',
+    gpu: '',
+    hdd: '',
+    ssd: '',
+  });
 
-	const tipos = [
-		{ nombre: 'Procesador', valor: 'cpu' },
-		{ nombre: 'Tarjeta Madre', valor: 'mb' },
-		{ nombre: 'Memoria RAM', valor: 'ram' },
-		{ nombre: 'Tarjeta Grafica', valor: 'gpu' },
-		{ nombre: 'HDD', valor: 'hdd' },
-		{ nombre: 'SSD', valor: 'ssd' },
-	];
+  const tipos = [
+    { nombre: 'Procesador', valor: 'cpu' },
+    { nombre: 'Tarjeta Madre', valor: 'mb' },
+    { nombre: 'Memoria RAM', valor: 'ram' },
+    { nombre: 'Tarjeta Grafica', valor: 'gpu' },
+    { nombre: 'HDD', valor: 'hdd' },
+    { nombre: 'SSD', valor: 'ssd' },
+  ];
 
-	const panes = tipos.map((tipo, index) => {
-		return {
-			menuItem: tipo.nombre,
-			render: () => (
-				<Tab.Pane id={index}>
-					<Parts parts={partes.filter((part) => part.tipo === tipo.valor)} />
-				</Tab.Pane>
-			),
-		};
-	});
+  const panes = tipos.map((tipo, index) => {
+    return {
+      menuItem: tipo.nombre,
+      render: () => (
+        <Tab.Pane id={index}>
+          <Parts parts={partes.filter((part) => part.tipo === tipo.valor)} />
+        </Tab.Pane>
+      ),
+    };
+  });
 
-	const resumen = {
-		menuItem: 'Resumen',
-		render: () => (
-			<Fragment>
-				{(precioTotal = 0)}
-				<div id='tabla'>
-					<img alt='logo' src={logo} />
-					<Table celled>
-						<Table.Header>
-							<Table.Row>
-								<Table.HeaderCell>Concepto</Table.HeaderCell>
-								<Table.HeaderCell>Unidades</Table.HeaderCell>
-								<Table.HeaderCell>Costo</Table.HeaderCell>
-							</Table.Row>
-						</Table.Header>
-						<Table.Body>
-							{Object.entries(ensamble).map((e) => {
-								if (e[1] !== '') {
-									precioTotal += parseInt(e[1].precio);
-									return (
-										<Table.Row>
-											<Table.Cell>{e[1].nombre}</Table.Cell>
-											<Table.Cell>1</Table.Cell>
-											<Table.Cell>${e[1].precio}</Table.Cell>
-										</Table.Row>
-									);
-								}
-								return null;
-							})}
-							<Table.Row>
-								<Table.Cell></Table.Cell>
-								<Table.Cell>Costo total</Table.Cell>
-								<Table.Cell>${precioTotal}</Table.Cell>
-							</Table.Row>
-						</Table.Body>
-					</Table>
-					<Label color='blue' style={{ float: 'right' }}>
-						Cotización hecha el dia: {getDate()}
-					</Label>
-				</div>
-				<div style={{ paddingTop: '10px' }}>
-					<Button size='large' primary onClick={getPDF}>
-						Descargar PDF
-					</Button>
-				</div>
-			</Fragment>
-		),
-	};
+  const resumen = {
+    menuItem: 'Resumen',
+    render: () => <Resumen />,
+  };
 
-	const getPDF = () => {
-		const table = document.getElementById('tabla');
-		html2pdf().from(table).save();
-	};
+  const [partes, setPartes] = useState([]);
 
-	const getDate = () => {
-		var today = new Date();
-		var dd = String(today.getDate()).padStart(2, '0');
-		var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
-		var yyyy = today.getFullYear();
-		today = mm + '/' + dd + '/' + yyyy;
-		return today;
-	};
+  const getParts = async () => {
+    const querySnapshot = await db.collection('partes').get();
+    const docs = [];
+    querySnapshot.forEach((doc) => {
+      docs.push({ ...doc.data(), id: doc.id, selected: false });
+    });
+    setPartes(docs);
+  };
 
-	const [partes, setPartes] = useState([]);
+  useEffect(() => {
+    getParts();
+  }, []);
 
-	const getParts = async () => {
-		const querySnapshot = await db.collection('partes').get();
-		const docs = [];
-		querySnapshot.forEach((doc) => {
-			docs.push({ ...doc.data(), id: doc.id, selected: false });
-		});
-		setPartes(docs);
-	};
+  useEffect(() => {}, [ensamble]);
 
-	useEffect(() => {
-		getParts();
-	}, []);
-
-	useEffect(() => {
-		console.log(ensamble);
-		//console.log(panes);
-	}, [ensamble]);
-
-	useEffect(() => {
-		getParts();
-	}, []);
-	const handleClick = () => {
-		let elementQuote, elementPDF, elementTab;
-		elementTab = document.getElementsByClassName('active item');
-		elementQuote = document.getElementById('stepQuote');
-		elementPDF = document.getElementById('stepPDF');
-		console.log(elementTab);
-		if (elementTab[0].text === 'Resumen') {
-			if (elementPDF.classList.contains('active')) {
-				return 0;
-			} else {
-				elementPDF.classList.toggle('active');
-				elementQuote.classList.toggle('active');
-			}
-		} else {
-			if (!elementQuote.classList.contains('active')) {
-				elementPDF.classList.toggle('active');
-				elementQuote.classList.toggle('active');
-			}
-		}
-	};
-	return (
-		<Container className='content'>
-			<Steps />
-			<EnsambleContext.Provider value={{ ensamble, setEnsamble }}>
-				<Tab
-					menu={{
-						inverted: true,
-						fluid: true,
-					}}
-					onTabChange={handleClick}
-					panes={panes.concat(resumen)}
-				/>
-			</EnsambleContext.Provider>
-		</Container>
-	);
+  useEffect(() => {
+    getParts();
+  }, []);
+  const handleClick = () => {
+    let elementQuote, elementPDF, elementTab;
+    elementTab = document.getElementsByClassName('active item');
+    elementQuote = document.getElementById('stepQuote');
+    elementPDF = document.getElementById('stepPDF');
+    console.log(elementTab);
+    if (elementTab[0].text === 'Resumen') {
+      if (elementPDF.classList.contains('active')) {
+        return 0;
+      } else {
+        elementPDF.classList.toggle('active');
+        elementQuote.classList.toggle('active');
+      }
+    } else {
+      if (!elementQuote.classList.contains('active')) {
+        elementPDF.classList.toggle('active');
+        elementQuote.classList.toggle('active');
+      }
+    }
+  };
+  return (
+    <Container className='content'>
+      <Steps />
+      <EnsambleContext.Provider value={{ ensamble, setEnsamble }}>
+        <Tab
+          menu={{
+            inverted: true,
+            fluid: true,
+          }}
+          onTabChange={handleClick}
+          panes={panes.concat(resumen)}
+        />
+      </EnsambleContext.Provider>
+    </Container>
+  );
 }
 
 // la funcion .onSnapshot ofrece datos en tiempo real
--- src/components/Ensamble/Resumen.js
+++ src/components/Ensamble/Resumen.js
@@ -1,14 +1,121 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { Button, Label, Table } from 'semantic-ui-react';
+import logo from '../Navbar/giant.png';
 
-export default function Resumen({ ensamble }) {
+import html2pdf from 'html2pdf.js';
+import { EnsambleContext } from './EnsambleContext';
+
+import { loadStripe } from '@stripe/stripe-js';
+// Make sure to call `loadStripe` outside of a component’s render to avoid
+// recreating the `Stripe` object on every render.
+const stripePromise = loadStripe(
+  'pk_test_51IujvAIqOpiH1XDDENLMzBB13hlBmoIdSsfU9fRAqGl3vbRtXRSp6wUzSCbtzot9CLdrEpRUwzJueomQQrutCaX00008spOYCP'
+);
+
+export default function Resumen() {
+  const { ensamble } = useContext(EnsambleContext);
+  let precioTotal = 0;
+
+  const getPDF = () => {
+    const table = document.getElementById('tabla');
+    html2pdf().from(table).save();
+  };
+
+  const getDate = () => {
+    var today = new Date();
+    var dd = String(today.getDate()).padStart(2, '0');
+    var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+    var yyyy = today.getFullYear();
+    today = mm + '/' + dd + '/' + yyyy;
+    return today;
+  };
+
+  const handlePay = async (event) => {
+    const stripe = await stripePromise;
+
+    let items = [];
+
+    let data = {
+      equipo: [],
+    };
+
+    Object.values(ensamble).forEach((part) => {
+      if (part.id) {
+        //items.push(part.precio);
+        data.equipo.push(part.id);
+      }
+    });
+
+    // Call your backend to create the Checkout Session
+    const response = await fetch('http://localhost:5000/checkout', {
+      method: 'POST',
+      body: JSON.stringify(data),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+
+    const session = await response.json();
+
+    // When the customer clicks on the button, redirect them to Checkout.
+    const result = await stripe.redirectToCheckout({
+      sessionId: session.id,
+    });
+
+    if (result.error) {
+      // If `redirectToCheckout` fails due to a browser or network
+      // error, display the localized error message to your customer
+      // using `result.error.message`.
+    }
+  };
+
   return (
-    <div>
-      <div>{ensamble.cpu}</div>
-      <div>{ensamble.mb}</div>
-      <div>{ensamble.ram}</div>
-      <div>{ensamble.gpu}</div>
-      <div>{ensamble.hdd}</div>
-      <div>{ensamble.ssd}</div>
-    </div>
+    <>
+      {(precioTotal = 0)}
+      <div id='tabla'>
+        <img alt='logo' src={logo} />
+        <Table celled>
+          <Table.Header>
+            <Table.Row>
+              <Table.HeaderCell>Concepto</Table.HeaderCell>
+              <Table.HeaderCell>Unidades</Table.HeaderCell>
+              <Table.HeaderCell>Costo</Table.HeaderCell>
+            </Table.Row>
+          </Table.Header>
+          <Table.Body>
+            {Object.entries(ensamble).map((e) => {
+              if (e[1] !== '') {
+                precioTotal += parseInt(e[1].precio);
+                return (
+                  <Table.Row>
+                    <Table.Cell>{e[1].nombre}</Table.Cell>
+                    <Table.Cell>1</Table.Cell>
+                    <Table.Cell>${e[1].precio}</Table.Cell>
+                  </Table.Row>
+                );
+              }
+              return null;
+            })}
+            <Table.Row>
+              <Table.Cell></Table.Cell>
+              <Table.Cell>Costo total</Table.Cell>
+              <Table.Cell>${precioTotal}</Table.Cell>
+            </Table.Row>
+          </Table.Body>
+        </Table>
+        <Label color='blue' style={{ float: 'right' }}>
+          Cotización hecha el dia: {getDate()}
+        </Label>
+      </div>
+      <div style={{ paddingTop: '10px' }}>
+        <Button size='large' primary onClick={getPDF}>
+          Descargar PDF
+        </Button>
+
+        <Button size='large' secondary onClick={handlePay}>
+          Pagar
+        </Button>
+      </div>
+    </>
   );
 }
