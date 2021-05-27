5814ca92eb56bdba352398137fb6ddfe04c5f631
IO <al18760117@ite.edu.mx>
2021-05-26 23:32:35 -0700

    🔀 Links ordenados

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -8,7 +8,9 @@ export default function Navbar() {
     <nav className='navbar'>
       <ul className='navbar-nav'>
         <li className='logo'>
-          <img alt='logo' src={logo} />
+          <Link to='/'>
+            <img alt='logo' src={logo} />
+          </Link>
         </li>
         <li className='nav-item'>
           <Link to={'/inicio'}>
@@ -29,7 +31,55 @@ export default function Navbar() {
                 ></path>
               </svg>
 
-              <span className='link-text'>Tienda</span>
+              <span className='link-text'>Inicio</span>
+            </span>
+          </Link>
+        </li>
+
+        <li className='nav-item'>
+          <Link to={'/ensamble'}>
+            <span className='nav-link'>
+              <svg
+                aria-hidden='true'
+                focusable='false'
+                data-prefix='fas'
+                data-icon='calculator'
+                className='svg-inline--fa fa-calculator fa-w-14'
+                role='img'
+                xmlns='http://www.w3.org/2000/svg'
+                viewBox='0 0 448 512'
+              >
+                <path
+                  fill='currentColor'
+                  d='M400 0H48C22.4 0 0 22.4 0 48v416c0 25.6 22.4 48 48 48h352c25.6 0 48-22.4 48-48V48c0-25.6-22.4-48-48-48zM128 435.2c0 6.4-6.4 12.8-12.8 12.8H76.8c-6.4 0-12.8-6.4-12.8-12.8v-38.4c0-6.4 6.4-12.8 12.8-12.8h38.4c6.4 0 12.8 6.4 12.8 12.8v38.4zm0-128c0 6.4-6.4 12.8-12.8 12.8H76.8c-6.4 0-12.8-6.4-12.8-12.8v-38.4c0-6.4 6.4-12.8 12.8-12.8h38.4c6.4 0 12.8 6.4 12.8 12.8v38.4zm128 128c0 6.4-6.4 12.8-12.8 12.8h-38.4c-6.4 0-12.8-6.4-12.8-12.8v-38.4c0-6.4 6.4-12.8 12.8-12.8h38.4c6.4 0 12.8 6.4 12.8 12.8v38.4zm0-128c0 6.4-6.4 12.8-12.8 12.8h-38.4c-6.4 0-12.8-6.4-12.8-12.8v-38.4c0-6.4 6.4-12.8 12.8-12.8h38.4c6.4 0 12.8 6.4 12.8 12.8v38.4zm128 128c0 6.4-6.4 12.8-12.8 12.8h-38.4c-6.4 0-12.8-6.4-12.8-12.8V268.8c0-6.4 6.4-12.8 12.8-12.8h38.4c6.4 0 12.8 6.4 12.8 12.8v166.4zm0-256c0 6.4-6.4 12.8-12.8 12.8H76.8c-6.4 0-12.8-6.4-12.8-12.8V76.8C64 70.4 70.4 64 76.8 64h294.4c6.4 0 12.8 6.4 12.8 12.8v102.4z'
+                ></path>
+              </svg>
+
+              <span className='link-text'>Cotizar</span>
+            </span>
+          </Link>
+        </li>
+
+        <li className='nav-item'>
+          <Link to={'/contacto'}>
+            <span className='nav-link'>
+              <svg
+                aria-hidden='true'
+                focusable='false'
+                data-prefix='fas'
+                data-icon='calculator'
+                className='svg-inline--fa fa-calculator fa-w-14'
+                role='img'
+                xmlns='http://www.w3.org/2000/svg'
+                viewBox='0 0 448 512'
+              >
+                <path
+                  fill='currentColor'
+                  d='M400 0H48C22.4 0 0 22.4 0 48v416c0 25.6 22.4 48 48 48h352c25.6 0 48-22.4 48-48V48c0-25.6-22.4-48-48-48zM128 435.2c0 6.4-6.4 12.8-12.8 12.8H76.8c-6.4 0-12.8-6.4-12.8-12.8v-38.4c0-6.4 6.4-12.8 12.8-12.8h38.4c6.4 0 12.8 6.4 12.8 12.8v38.4zm0-128c0 6.4-6.4 12.8-12.8 12.8H76.8c-6.4 0-12.8-6.4-12.8-12.8v-38.4c0-6.4 6.4-12.8 12.8-12.8h38.4c6.4 0 12.8 6.4 12.8 12.8v38.4zm128 128c0 6.4-6.4 12.8-12.8 12.8h-38.4c-6.4 0-12.8-6.4-12.8-12.8v-38.4c0-6.4 6.4-12.8 12.8-12.8h38.4c6.4 0 12.8 6.4 12.8 12.8v38.4zm0-128c0 6.4-6.4 12.8-12.8 12.8h-38.4c-6.4 0-12.8-6.4-12.8-12.8v-38.4c0-6.4 6.4-12.8 12.8-12.8h38.4c6.4 0 12.8 6.4 12.8 12.8v38.4zm128 128c0 6.4-6.4 12.8-12.8 12.8h-38.4c-6.4 0-12.8-6.4-12.8-12.8V268.8c0-6.4 6.4-12.8 12.8-12.8h38.4c6.4 0 12.8 6.4 12.8 12.8v166.4zm0-256c0 6.4-6.4 12.8-12.8 12.8H76.8c-6.4 0-12.8-6.4-12.8-12.8V76.8C64 70.4 70.4 64 76.8 64h294.4c6.4 0 12.8 6.4 12.8 12.8v102.4z'
+                ></path>
+              </svg>
+
+              <span className='link-text'>Contacto</span>
             </span>
           </Link>
         </li>
@@ -54,78 +104,6 @@ export default function Navbar() {
               </svg>
 
               <span className='link-text'>Agregar</span>
-            </span>
-          </Link>
-        </li>
-
-        {/* <li className='nav-item'>
-          <Link to={'/cotizar'}>
-            <span className='nav-link'>
-              <svg
-                aria-hidden='true'
-                focusable='false'
-                data-prefix='fas'
-                data-icon='calculator'
-                className='svg-inline--fa fa-calculator fa-w-14'
-                role='img'
-                xmlns='http://www.w3.org/2000/svg'
-                viewBox='0 0 448 512'
-              >
-                <path
-                  fill='currentColor'
-                  d='M400 0H48C22.4 0 0 22.4 0 48v416c0 25.6 22.4 48 48 48h352c25.6 0 48-22.4 48-48V48c0-25.6-22.4-48-48-48zM128 435.2c0 6.4-6.4 12.8-12.8 12.8H76.8c-6.4 0-12.8-6.4-12.8-12.8v-38.4c0-6.4 6.4-12.8 12.8-12.8h38.4c6.4 0 12.8 6.4 12.8 12.8v38.4zm0-128c0 6.4-6.4 12.8-12.8 12.8H76.8c-6.4 0-12.8-6.4-12.8-12.8v-38.4c0-6.4 6.4-12.8 12.8-12.8h38.4c6.4 0 12.8 6.4 12.8 12.8v38.4zm128 128c0 6.4-6.4 12.8-12.8 12.8h-38.4c-6.4 0-12.8-6.4-12.8-12.8v-38.4c0-6.4 6.4-12.8 12.8-12.8h38.4c6.4 0 12.8 6.4 12.8 12.8v38.4zm0-128c0 6.4-6.4 12.8-12.8 12.8h-38.4c-6.4 0-12.8-6.4-12.8-12.8v-38.4c0-6.4 6.4-12.8 12.8-12.8h38.4c6.4 0 12.8 6.4 12.8 12.8v38.4zm128 128c0 6.4-6.4 12.8-12.8 12.8h-38.4c-6.4 0-12.8-6.4-12.8-12.8V268.8c0-6.4 6.4-12.8 12.8-12.8h38.4c6.4 0 12.8 6.4 12.8 12.8v166.4zm0-256c0 6.4-6.4 12.8-12.8 12.8H76.8c-6.4 0-12.8-6.4-12.8-12.8V76.8C64 70.4 70.4 64 76.8 64h294.4c6.4 0 12.8 6.4 12.8 12.8v102.4z'
-                ></path>
-              </svg>
-
-              <span className='link-text'>Cotizar</span>
-            </span>
-          </Link>
-        </li> */}
-
-        <li className='nav-item'>
-          <Link to={'/ensamble'}>
-            <span className='nav-link'>
-              <svg
-                aria-hidden='true'
-                focusable='false'
-                data-prefix='fas'
-                data-icon='calculator'
-                className='svg-inline--fa fa-calculator fa-w-14'
-                role='img'
-                xmlns='http://www.w3.org/2000/svg'
-                viewBox='0 0 448 512'
-              >
-                <path
-                  fill='currentColor'
-                  d='M400 0H48C22.4 0 0 22.4 0 48v416c0 25.6 22.4 48 48 48h352c25.6 0 48-22.4 48-48V48c0-25.6-22.4-48-48-48zM128 435.2c0 6.4-6.4 12.8-12.8 12.8H76.8c-6.4 0-12.8-6.4-12.8-12.8v-38.4c0-6.4 6.4-12.8 12.8-12.8h38.4c6.4 0 12.8 6.4 12.8 12.8v38.4zm0-128c0 6.4-6.4 12.8-12.8 12.8H76.8c-6.4 0-12.8-6.4-12.8-12.8v-38.4c0-6.4 6.4-12.8 12.8-12.8h38.4c6.4 0 12.8 6.4 12.8 12.8v38.4zm128 128c0 6.4-6.4 12.8-12.8 12.8h-38.4c-6.4 0-12.8-6.4-12.8-12.8v-38.4c0-6.4 6.4-12.8 12.8-12.8h38.4c6.4 0 12.8 6.4 12.8 12.8v38.4zm0-128c0 6.4-6.4 12.8-12.8 12.8h-38.4c-6.4 0-12.8-6.4-12.8-12.8v-38.4c0-6.4 6.4-12.8 12.8-12.8h38.4c6.4 0 12.8 6.4 12.8 12.8v38.4zm128 128c0 6.4-6.4 12.8-12.8 12.8h-38.4c-6.4 0-12.8-6.4-12.8-12.8V268.8c0-6.4 6.4-12.8 12.8-12.8h38.4c6.4 0 12.8 6.4 12.8 12.8v166.4zm0-256c0 6.4-6.4 12.8-12.8 12.8H76.8c-6.4 0-12.8-6.4-12.8-12.8V76.8C64 70.4 70.4 64 76.8 64h294.4c6.4 0 12.8 6.4 12.8 12.8v102.4z'
-                ></path>
-              </svg>
-
-              <span className='link-text'>Ensamble</span>
-            </span>
-          </Link>
-        </li>
-
-        <li className='nav-item'>
-          <Link to={'/contacto'}>
-            <span className='nav-link'>
-              <svg
-                aria-hidden='true'
-                focusable='false'
-                data-prefix='fas'
-                data-icon='calculator'
-                className='svg-inline--fa fa-calculator fa-w-14'
-                role='img'
-                xmlns='http://www.w3.org/2000/svg'
-                viewBox='0 0 448 512'
-              >
-                <path
-                  fill='currentColor'
-                  d='M400 0H48C22.4 0 0 22.4 0 48v416c0 25.6 22.4 48 48 48h352c25.6 0 48-22.4 48-48V48c0-25.6-22.4-48-48-48zM128 435.2c0 6.4-6.4 12.8-12.8 12.8H76.8c-6.4 0-12.8-6.4-12.8-12.8v-38.4c0-6.4 6.4-12.8 12.8-12.8h38.4c6.4 0 12.8 6.4 12.8 12.8v38.4zm0-128c0 6.4-6.4 12.8-12.8 12.8H76.8c-6.4 0-12.8-6.4-12.8-12.8v-38.4c0-6.4 6.4-12.8 12.8-12.8h38.4c6.4 0 12.8 6.4 12.8 12.8v38.4zm128 128c0 6.4-6.4 12.8-12.8 12.8h-38.4c-6.4 0-12.8-6.4-12.8-12.8v-38.4c0-6.4 6.4-12.8 12.8-12.8h38.4c6.4 0 12.8 6.4 12.8 12.8v38.4zm0-128c0 6.4-6.4 12.8-12.8 12.8h-38.4c-6.4 0-12.8-6.4-12.8-12.8v-38.4c0-6.4 6.4-12.8 12.8-12.8h38.4c6.4 0 12.8 6.4 12.8 12.8v38.4zm128 128c0 6.4-6.4 12.8-12.8 12.8h-38.4c-6.4 0-12.8-6.4-12.8-12.8V268.8c0-6.4 6.4-12.8 12.8-12.8h38.4c6.4 0 12.8 6.4 12.8 12.8v166.4zm0-256c0 6.4-6.4 12.8-12.8 12.8H76.8c-6.4 0-12.8-6.4-12.8-12.8V76.8C64 70.4 70.4 64 76.8 64h294.4c6.4 0 12.8 6.4 12.8 12.8v102.4z'
-                ></path>
-              </svg>
-
-              <span className='link-text'>Cotizar</span>
             </span>
           </Link>
         </li>
