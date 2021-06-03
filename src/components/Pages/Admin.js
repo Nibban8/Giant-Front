f3bb43c996954378b01d89f3fa937e292846bbcf
Symphatico <Symphatico>
2021-06-02 18:39:27 -0700

    asd

--- src/components/Pages/Admin.js
+++ src/components/Pages/Admin.js
@@ -1,6 +1,5 @@
 import React, { Fragment } from 'react';
 import { Button, Grid } from 'semantic-ui-react';
-import Alertas from '../Administrador/Alertas';
 import { Link } from 'react-router-dom';
 
 export default function Dashboard() {
